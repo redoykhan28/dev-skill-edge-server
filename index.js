cdc50361100be1fc0f9547c632b5a571d9a0e296
unknown <redoyk2015@gmail.com>
2022-10-25 01:44:56 +0600

    server link created

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 const catagories = require('./data/categories.json')
+const course = require('./data/course.json')
 
 app.use(cors())
 
@@ -14,10 +15,34 @@ app.get('/', (req, res) => {
     res.send('Skill Edge running')
 })
 
+//For category menu
 app.get('/categories', (req, res) => {
 
     res.send(catagories);
 
+})
+
+//For all course
+app.get('/course', (req, res) => {
+
+    res.send(course);
+
+})
+
+//For course by category
+app.get('/courseByCategory/:id', (req, res) => {
+
+    let id = req.params.id;
+    let selectedCourse = course.filter(c => c.category_id === id)
+    res.send(selectedCourse)
+})
+
+//For course Details
+app.get('/courseDetails/:id', (req, res) => {
+
+    let id = req.params.id;
+    let courseDetails = course.find(c => c._id === id)
+    res.send(courseDetails)
 })
 
 
